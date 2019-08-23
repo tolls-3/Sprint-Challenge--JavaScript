95f24e2250526cebbc1906d32a69fe9b99a1f0be
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-08-23 09:29:37 +0100

    working on object-arrays.js:created roar method

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,7 +12,10 @@ const tyrannosaurus = {
   diet:"carnivorous",
   weight:"7000kg",
   length:"12m",
-  period:"Late Cretaceous"
+  period:"Late Cretaceous",
+  roar:function(){
+    return "RAWERSRARARWERSARARARRRR!";
+  }
 };
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -49,7 +52,7 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
